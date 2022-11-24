b82374a32d4df5747b502905231291b5f1b8eaf9
user <user@users-MacBook-Pro.local>
2022-11-23 22:32:07 -0300

    trabalhoPim

---
+++ projetoPim.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void menu()
+{
+    printf("Selecion uma opção do sistema: \n");
+    printf("\t1- Sobre a empresa. \n");
+    printf("\t2- Cadastrar usuario. \n");
+    printf("\t3- Lista de Usuarios no sistema. \n");
+}
+
+int main() {
+    menu();
+
+    return 0;
+}
+
